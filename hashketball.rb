5706d821d21119a950df27438b707d6b11db5f52
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-15 17:27:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,19 @@ def game_hash
     :home => {
       :team_name => "",
       :colors => [],
-      :players => []
+      :players => [ {
+        :player_name = "Alan Anderson"
+        :number = "0"
+        :shoe = "16"
+        :points = "22"
+        :rebounds = "12"
+        :assists = "12"
+        :steals = "3"
+        :blocks = "1"
+        :slam_dunks = "1"
+
+         }
+]
     },
     :away => {
 
